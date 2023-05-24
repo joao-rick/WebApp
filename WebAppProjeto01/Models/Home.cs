a5584d30446a8f9767d3c1ecb3ede8d49db086d3
João Ricardo <101953125+joao-rick@users.noreply.github.com>
2023-05-23 22:35:49 -0300

    Add files via upload

---
+++ WebAppProjeto01/Models/Home.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto01.Models
+{
+    public class Home
+    {
+        public IEnumerable<Fabricante> Fabricantes { get; set; }
+        public IEnumerable<Categoria> Categorias { get; set; }
+    }
+}
