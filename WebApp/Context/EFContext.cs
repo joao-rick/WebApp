a784c184b94e4b7b310048e550209264d93b5019
João Ricardo <101953125+joao-rick@users.noreply.github.com>
2023-06-04 21:27:14 -0300

    Add files via upload

---
+++ WebApp/Context/EFContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApp.Models;
+
+namespace WebApp.Context
+{
+public class EFContext : DbContext
+{
+public EFContext() : base("WebAppDB") {
+Database.SetInitializer<EFContext>(
+new DropCreateDatabaseIfModelChanges<EFContext>());
+}
+public DbSet<Categoria> Categorias { get; set; }
+public DbSet<Fabricante> Fabricantes { get; set; }
+public DbSet<Produto> Produtos { get; set; }
+}
+}
---
+++ WebApp/Controllers/CategoriasController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Context;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly EFContext context = new EFContext();
+        public ActionResult Index()
+        {
+            return View(context.Categorias.OrderBy(c => c.Nome));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Categorias.Add(categoria);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(categoria).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = context.Categorias.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Categoria categoria = context.Categorias.Find(id);
+            context.Categorias.Remove(categoria);
+            context.SaveChanges();
+            TempData["Message"] = "Categoria " + categoria.Nome.ToUpper() + " foi removido";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
