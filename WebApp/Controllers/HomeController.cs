e5c29c62e67d2602b50ca4f2df11b215884a4a0c
João Ricardo Fernandes de Almeida <almeida.joao@academico.ifrn.edu.br>
2023-06-13 15:46:37 -0300

    Lista 4 - cap 4

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using WebApp.Context;
 using WebApp.Models;
+using System.Data.Entity;
 
 namespace WebApp.Controllers
 {
@@ -20,5 +21,28 @@ namespace WebApp.Controllers
             var home = new Home() { Fabricantes = fabricantes, Categorias = categorias };
             return View(home);
         }
+
+        
+        public ActionResult Categoria(int id)
+        {
+            var categorias = context.Categorias.Include("Produtos.Categoria");
+            var fabricantes = context.Fabricantes.Include("Produtos.Fabricante");
+            var home = new Home() { Fabricantes = fabricantes, Categorias = categorias };
+            ViewData["item"] = categorias.Where(c => c.CategoriaId == id).First().Produtos;
+            ViewData["categoria"] = true;
+            return View("Index", home);
+        }
+
+        public ActionResult Fabricante(int id)
+        {
+            var categorias = context.Categorias.Include("Produtos.Categoria");
+            var fabricantes = context.Fabricantes.Include("Produtos.Fabricante");
+            var home = new Home() { Fabricantes = fabricantes, Categorias = categorias };
+            ViewData["item"] = fabricantes.Where(f => f.FabricanteId == id).First().Produtos;
+            ViewData["categoria"] = false;
+            return View("Index", home);
+        }
     }
+
+
 }
